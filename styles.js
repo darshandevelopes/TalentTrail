f89b05d9bb2d342dd3ff1296a377b3a19e10c5d8
Darshan Rahate <heyycreazy@gmail.com>
2023-12-13 17:00:22 +0530

    Added styles.js for shared styles between components

---
+++ styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const sharedStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  titleBold: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+
+  primaryBold: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+
+  secondaryBold: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  secondaryNormal: {
+    fontSize: 14,
+  },
+});
